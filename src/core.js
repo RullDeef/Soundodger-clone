3a693b75db3c98e9b48ddf149a4ef1e350d0a115
RullDeef <34412244+RullDeef@users.noreply.github.com>
2018-08-12 18:24:07 +0600

    Start writing player object

--- src/core.js
+++ src/core.js
@@ -1,57 +1,15 @@
 /*
-    This file contains Input structure for handling touch input. 
+    This file contains all game elements.
 */
 
-const { Vector2 } = THREE;
+const { Vector2, Vector3 } = THREE;
 
-var Input = {
-    touchPos: new Vector2,
-    delta: new Vector2,
-    _clickArr: [], 
-    
-    init: function() {
-        window.addEventListener('touchstart', e => this.start(e));
-        window.addEventListener('touchmove', e => this.update(e));
-        
-        window.addEventListener('click', e => {
-            this.click(e);
-            this._clickArr.forEach(ob => ob.f(...ob.args));
-        });
+var Player = {
+    material: null, geometry: null, mesh: null,
+
+    init: function(radius = 0.2) {
+        Player.material = new THREE.MeshBasicMaterial({ color: 0x446600 });
+        Player.geometry = new THREE.CircleGeometry(radius, 30);
+        Player.mesh = new THREE.Mesh(Player.geometry, Player.material);
     }, 
-    
-    start: function(event) {
-        let x = event.touches[0].pageX;
-        let y = event.touches[0].pageY;
-        x = x/window.innerWidth*2 - 1;
-        y = 1 - y/window.innerHeight*2;
-        
-        this.touchPos.set(x, y);
-        this.delta.set(0, 0);
-    }, 
-    
-    update: function(event) {
-        let x = event.touches[0].pageX;
-        let y = event.touches[0].pageY;
-        x = x/window.innerWidth*2 - 1;
-        y = 1 - y/window.innerHeight*2;
-        
-        this.delta.x = x - this.touchPos.x
-        this.delta.y = y - this.touchPos.y;
-        this.touchPos.set(x, y);
-    }, 
-    
-    click: function(event) {
-        let x = event.pageX;
-        let y = event.pageY;
-        
-        x = x/window.innerWidth*2 - 1;
-        y = 1 - y/window.innerHeight*2;
-        
-        this.touchPos.set(x, y);
-        this.delta.set(0, 0);
-    }, 
-    
-    onclick: function(f, ...args) {
-        this._clickArr.push({f, args});
-    }
 }
